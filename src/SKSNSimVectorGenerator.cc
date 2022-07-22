c72d5765b34b96837a6ead68068531ab6a46e065
Shota Izumiyama <izumiyama@hep.phys.titech.ac.jp>
2022-07-22 14:32:00 +0900

    Constructed Vector and Matrix for linear algebra

--- src/SKSNSimVectorGenerator.cc
+++ src/SKSNSimVectorGenerator.cc
@@ -15,14 +15,57 @@ class UtilVector3 {
     T x,y,z;
     UtilVector3(T xx, T yy, T zz) : x(xx), y(yy), z(zz){}
     T operator[](size_t i)const { if(i==0) return x; if(i==1) return y; if(i==2) return z; return 9999.;}
-//    UtilVector3 operator*(const T a){ return UtilVector3( a* x, a* y, a*z); }
+    T operator*(const UtilVector3 &v) const { return v.x * x + v.y * y + v.z*z; }
     UtilVector3 operator+(const UtilVector3 v) const { return UtilVector3( v.x + x,  v.y+ y, v.z+z); }
     UtilVector3 operator-(const UtilVector3 v) const { return -1.0 * v + *this; }
-    T Mag2() const { return x*x + y*y + z*z; }
+    T Mag2() const { return *this * *this; }
     T Mag() const { return std::sqrt(Mag2()); }
     UtilVector3 Unit() const { return (1./Mag())* *this ; }
 };
 template <class T> UtilVector3<T> operator*(T a, const UtilVector3<T> &v){ return UtilVector3<T>(a*v.x, a*v.y, a*v.z);}
+
+template<class T>
+class UtilMatrix3 {
+  public:
+    T v[3][3];
+    UtilMatrix3(){}
+    ~UtilMatrix3(){}
+    UtilMatrix3(
+        T x00, T x01, T x02,
+        T x10, T x11, T x12,
+        T x20, T x21, T x22
+        ){
+      v[0][0] = x00; v[0][1] = x01; v[0][2] = x02;
+      v[1][0] = x10; v[1][1] = x11; v[1][2] = x12;
+      v[2][0] = x20; v[2][1] = x21; v[2][2] = x22;
+    }
+    UtilMatrix3(
+        UtilVector3<T> v0, UtilVector3<T> v1, UtilVector3<T> v2
+        ){
+      v[0][0] = v0.x; v[0][1] = v1.x; v[0][2] = v2.x;
+      v[1][0] = v0.y; v[1][1] = v1.y; v[1][2] = v2.y;
+      v[2][0] = v0.z; v[2][1] = v1.z; v[2][2] = v2.z;
+    }
+    UtilVector3<T> operator*(const UtilVector3<T> &l) const {
+      T x[3];
+      for(int i = 0; i < 3; i++){
+        x[i] = UtilVector3<T>(v[i][0], v[i][1], v[i][2]) * l;
+      }
+      return UtilVector3<T>(x[0], x[1], x[2]);
+    }
+
+    UtilMatrix3 operator*(const UtilMatrix3 &m) const {
+      auto r = UtilMatrix3();
+      for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+          r[i][j] = 0;
+          for(int k = 0; k < 3; k++)
+            r[i][j] += this->v[i][k] * m.v[k][j];
+        }
+      }
+      return r;
+    }
+};
 
 double SKSNSimVectorGenerator::FindMaxProb ( SKSNSimFluxModel &flux, SKSNSimCrosssectionModel &xsec){
   double maxP = 0.;
@@ -82,24 +125,15 @@ SKSNSimSNEventVector SKSNSimVectorGenerator::GenerateEventIBD() {
   //double nuDir[3];
   double theta = getRandomReal( 0., 1.) * M_PI;
   double phi = getRandomReal( 0., 1.) * 2. * M_PI;
-  UtilVector3<double> nuDir (
-      std::sin(theta) * std::cos(phi),
-      sin( theta ) * sin( phi ),
-      cos( theta )
-      );
+  UtilVector3<double> nuDir ( std::sin(theta)*std::cos(phi),
+                                        sin(theta)*sin(phi),
+                                                 cos(theta));
 
 
   // Rotation matrix of neutrino direction
-  double Rmat[3][3];
-  Rmat[0][0] = cos( theta ) * cos( phi );
-  Rmat[0][1] = -sin( phi );
-  Rmat[0][2] = sin( theta ) * cos( phi );
-  Rmat[1][0] = cos( theta ) * sin( phi );
-  Rmat[1][1] = cos( phi );
-  Rmat[1][2] = sin( theta ) * sin( phi );
-  Rmat[2][0] = -sin( theta );
-  Rmat[2][1] = 0.;
-  Rmat[2][2] = cos( theta );
+  UtilMatrix3<double> Rmat( cos(theta)*cos(phi), -sin(phi), sin(theta)*cos(phi),
+                            cos(theta)*sin(phi),  cos(phi), sin(theta)*sin(phi),
+                                    -sin(theta),        0.,          cos(theta));
 
   // interaction point
   auto determinePosition = std::bind([](TRandom &rng)
@@ -153,19 +187,7 @@ SKSNSimSNEventVector SKSNSimVectorGenerator::GenerateEventIBD() {
       sin( eTheta ) * sin( ePhi ),
       cos( eTheta ));
 
-  UtilVector3<double> eDir(0., 0., 0.);
-  {
-    double eDirBuf[3];
-    eDirBuf[0] = 0.;
-    eDirBuf[1] = 0.;
-    eDirBuf[2] = 0.;
-    for( int i = 0; i < 3; i++ ){
-      for( int j = 0; j < 3; j++ ){
-        eDirBuf[i] += Rmat[i][j] * origVec[j];
-      }
-    }
-    eDir = UtilVector3<double>(eDirBuf[0], eDirBuf[1], eDirBuf[2]);
-  }
+  const UtilVector3<double> eDir = Rmat * origVec;
 
   //------------------------------------------------------------------------
 
