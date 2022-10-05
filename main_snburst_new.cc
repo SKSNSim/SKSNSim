3ae638c77da38211a4077bac11e35e4ae360852b
Shota Izumiyama <izumiyama@hep.phys.titech.ac.jp>
2022-10-05 19:50:34 +0900

    Main program for new structured SKSNSim

---
+++ main_snburst_new.cc
@@ -0,0 +1,59 @@
+#include "SKSNSimVectorGenerator.hh"
+#include <memory>
+#include <TRandom3.h>
+#include "SKSNSimTools.hh"
+
+int main( int argc, char ** argv )
+{
+
+	//std::cout<<" Start Generating Event "<< argc <<std::endl;
+
+	//if(argc < 5){
+	//  std::cerr<<" Argument for "<< argv[0] << std::endl;
+	//  std::cerr<<" 1st: model name (e.g. nakazato/intp2001.data)" << std::endl;
+	//  std::cerr<<" 2nd: neutrino-oscillation (0:no, 1:normal, 2:inverted)"<< std::endl;
+	//  std::cerr<<" 3rd: Distance (normalized to 10kpc)"<< std::endl;
+	//  std::cerr<<" 4th: Generate event or not (0 : No just calculate expected, 1: Yes for detector simulation)"<< std::endl;
+	//  std::cerr<<" 5th: Output directory, default: ./data/(SN model)"<< std::endl;
+	//  std::cerr<<" 6th: Random seed"<< std::endl;
+	//  std::cerr<<" return -1"<<std::endl;
+	//  exit(1);
+	//}
+
+	///*-----set model name-----*/
+	//std::string ModelName = std::string(argv[1]);
+
+	///*-----neutrino oscillation-----*/
+	//int nuosc = atoi(argv[2]);
+
+	///*-----Distance-----*/
+	//double distIO = atof(argv[3]); // normalized to 10kpc
+
+	///*-----Generate events or not-----*/
+	//int flagIO = atoi(argv[4]);
+
+	///*-----Output directory-----*/
+	//std::string OutDirIO("./data");
+	//if(argc > 5) OutDirIO = std::string(argv[5]);
+
+	///*-----random number initialization-----*/
+	//uint seedIO = 0;
+	//if(argc > 6) seedIO = atoi(argv[6]);
+  //
+  std::unique_ptr<TRandom3> rng = std::make_unique<TRandom3>(42);
+  std::unique_ptr<SKSNSimFluxModel> flux_nakazato (new SKSNSimSNFluxNakazato());
+  
+
+	/*-----Geneartion-----*/
+  std::unique_ptr<SKSNSimVectorSNGenerator> generator = std::make_unique<SKSNSimVectorSNGenerator>();
+  generator->SetRandomGenerator(std::move(rng));
+  generator->AddFluxModel(std::move(flux_nakazato));
+  auto buffer = generator->GenerateEvents();
+  SKSNSimTools::DumpDebugMessage(Form("Successed GenerateEvents -> %d events", (int)buffer.size()));
+
+  SKSNSimTools::DumpDebugMessage("Start to delete explictly");
+  generator.reset();
+  SKSNSimTools::DumpDebugMessage("Finished to delete explictly");
+
+  return EXIT_SUCCESS;
+}
